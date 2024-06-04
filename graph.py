6da332dae7fe3ea4616ebee86ac22043f3173c61
flheight <75139846+flheight@users.noreply.github.com>
2024-06-04 14:49:38 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -7,30 +7,11 @@ class Graph:
     def __init__(self, n_classes):
         self.n_classes = n_classes
 
-    def fit(self, X, n_nodes, lam=5e-2, M=1e16):
+    def fit(self, X, n_nodes, M=1e16):
         kmeans = KMeans(n_clusters=n_nodes).fit(X)
 
-        affinity = .5 * lam * pairwise_distances(kmeans.cluster_centers_, metric='sqeuclidean')
+        affinity = pairwise_distances(kmeans.cluster_centers_, metric='sqeuclidean')
         np.fill_diagonal(affinity, np.inf)
-
-        for i in range(n_nodes):
-            for j in range(i):
-                data = np.vstack((X[kmeans.labels_ == i], X[kmeans.labels_ == j]))
-
-                diffs_null = data - np.vstack((kmeans.cluster_centers_[i], kmeans.cluster_centers_[j]))[:, np.newaxis]
-                dists_null = np.min(np.einsum('ijk,ijk->ij', diffs_null, diffs_null), axis=0)
-                inertia_null = dists_null.mean()
-
-                segment = kmeans.cluster_centers_[j] - kmeans.cluster_centers_[i]
-                dots = np.dot(data - kmeans.cluster_centers_[i], segment) / np.dot(segment, segment)
-                projs = kmeans.cluster_centers_[i] + np.outer(np.clip(dots, 0, 1), segment)
-                diffs_alt = data - projs
-                dists_alt = np.einsum('ij,ij->i', diffs_alt, diffs_alt)
-                inertia_alt = dists_alt.mean()
-
-                affinity[i, j] += inertia_alt - inertia_null
-
-        affinity += affinity.T
 
         q1 = np.quantile(affinity, .25)
         q3 = np.quantile(affinity, .75)
